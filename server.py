bb52aecd064c50e96736bb47f9f6789a1151872a
Gergo1212 <gergo.meszaros.codecool@gmail.com>
2019-10-29 14:44:09 +0100

    temp

--- server.py
+++ server.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/list')
 def route_list():
     return render_template("list.html")
+
 
 if __name__ == '__main__':
     app.run(
