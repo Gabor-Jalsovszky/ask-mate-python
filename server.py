11221f0739118cda7e54e3604f2a9c701409f13b
Gabor-Jalsovszky <gabor.jalsovszky@gmail.com>
2019-10-29 14:44:03 +0100

    Add more subpages to server.py

--- server.py
+++ server.py
@@ -7,6 +7,18 @@ app = Flask(__name__)
 def route_list():
     return render_template("list.html")
 
+@app.route('/add-question')
+def route_add_question():
+    return render_template("question.html")
+
+@app.route('/question/<question_id>')
+def route_question(question_id):
+    return render_template("question.html")
+
+@app.route('/question/<question_id>/new-answer')
+def route_new_answer(question_id):
+    return render_template("question.html")
+
 if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
