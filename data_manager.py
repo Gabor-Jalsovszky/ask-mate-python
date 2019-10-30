376fb86075b2713d71615794d03fc87bdc546321
Gabor-Jalsovszky <gabor.jalsovszky@gmail.com>
2019-10-30 13:44:29 +0100

    Add get_all_questions function to data_handler

--- data_manager.py
+++ data_manager.py
@@ -1,11 +1,10 @@
 import csv
 
 def get_all_questions():
-    questions = {}
-    #with open('question.csv', 'r') as questions_file:
-        #for line in csv.DictReader(questions_file)
-        #questions =
-    return
+    with open('sample_data/question.csv', 'r') as questions_file:
+        reader = csv.DictReader(questions_file)
+        questions = list(reader)
+    return questions
 
 def add_new_question():
 
