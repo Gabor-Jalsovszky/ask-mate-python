95571eb2b233fc36f05e1d12bda3d5feafe1dde6
Gabor-Jalsovszky <gabor.jalsovszky@gmail.com>
2019-10-30 14:31:10 +0100

    Add ID counter

--- data_manager.py
+++ data_manager.py
@@ -1,14 +1,23 @@
 import csv
 
+fieldnames = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
+
 def get_all_questions():
     with open('sample_data/question.csv', 'r') as questions_file:
         reader = csv.DictReader(questions_file)
         questions = list(reader)
     return questions
 
-def add_new_question():
-
-    return
+def add_new_question(question):
+    with open('sample_data/question.csv', 'r') as questions_file:
+        max_id = 0
+        reader = csv.DictReader(questions_file)
+        for line in reader:
+            if line['id'] >= max_id:
+                max_id = line['id']
+    with open('sample_data/question.csv', 'a') as questions_file:
+        writer = csv.DictWriter(questions_file, fieldnames=fieldnames)
+        writer.writerow({'id': int(max_id) + 1, title': question[question_title], 'message': question[question]})
 
 def add_new_answer():
 
--- server.py
+++ server.py
@@ -15,6 +15,8 @@ def route_list():
 @app.route('/add-question', methods=['GET', 'POST'])
 def route_add_question():
     if request.method == 'POST':
+        question = request.form
+        data_manager.add_new_question(question)
         return redirect('/list')
     elif request.method == 'GET':
         return render_template("question.html")
